4ee07f12050c61a37cd2bae2d975358db682da5c
Roberto <robertoea97@gmail.com>
2025-10-26 12:09:34 +0100

    Arreglos principio app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,56 +1,44 @@
 plugins {
-    id "com.android.application"
-    id "kotlin-android"
+    id("com.android.application")
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id "dev.flutter.flutter-gradle-plugin"
+    id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.pdf_scanner.app"
+    namespace = "es.centraldelregalo.pdf_scanner"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        coreLibraryDesugaringEnabled true
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.pdf_scanner.app"
+        applicationId = "es.centraldelregalo.pdf_scanner"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        multiDexEnabled true
-        minSdkVersion 23
     }
 
     buildTypes {
         release {
-            minifyEnabled false
-            shrinkResources false
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.debug
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
 flutter {
-    source '../..'
-}
-
-dependencies {
-    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.1.4'
-    implementation "androidx.multidex:multidex:2.0.1"
-    implementation 'com.google.android.material:material:1.9.0'
-    implementation 'androidx.concurrent:concurrent-futures:1.1.0'
+    source = "../.."
 }
---
+++ android/app/src/main/kotlin/es/centraldelregalo/pdf_scanner/MainActivity.kt
@@ -0,0 +1,5 @@
+package es.centraldelregalo.pdf_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
